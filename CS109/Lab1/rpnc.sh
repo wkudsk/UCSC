829e0747edeafcab3cad0ea01d1c9e1eefc32b3e
William Kudsk <wkudsk@gmail.com>
2018-04-05 14:58:54 -0700

    Finished the bash part

--- CS109/Lab1/rpnc.sh
+++ CS109/Lab1/rpnc.sh
@@ -4,6 +4,7 @@
 #
 # Accepts an single argument a RPN, evaluates it and prints the answer to stdout.
 # Using code shown to us in David Harrison's lecture
+# See the webcast at http://opencast-util.lt.ucsc.edu/search/?seriestitle=CMPS-109-LEC-01-2182-61545
 
 stack=()
 next=0
@@ -18,12 +19,13 @@ do
 	then
 		stack[$next]=$oper
 	else
-		#Else, its an operator. Pop last two from stack and perform operation.
+		#Else, its an operator. Pop last two from the stack and perform operation.
 		(( next -= 1 ))
 		oper_2=${stack[$next]}
 		(( next -= 1 ))
 		oper_1=${stack[$next]}
 
+		#List of if statements check to see which operator it is.
 		if [[ "$oper" == "+" ]]
 		then
 			(( value = $oper_1 + $oper_2 ))
