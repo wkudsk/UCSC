87282244ed688de10b9d6dcd55a0cd67db70172e
William Kudsk <wkudsk@gmail.com>
2018-04-05 19:04:35 -0700

    Trying to get python code working

--- CS109/Lab1/rpnc.py
+++ CS109/Lab1/rpnc.py
@@ -3,4 +3,34 @@
 #
 # Accepts an single argument in RPN, evaluates it and prints the answer to stdout.
 # 
-print("not implemented")
+if __name__ == '__main__':
+	stack = []
+	string = str(input())
+	#http://www.pythonforbeginners.com/dictionary/python-split
+	array = string.split(":")
+	print(array[0])
+	array = array[0].split(" ")
+	for word in array:
+		
+		#https://stackoverflow.com/questions/5424716/how-to-check-if-string-input-is-a-number
+		if(word.isdigit()):
+			stack.append(int(word))
+		
+		elif(word != " " and word != ":"):
+			b = stack.pop()
+			a = stack.pop()
+			result = 0
+
+			if(word == '+'):
+				result = a + b
+			elif(word == '-'):
+				result = a - b
+			elif(word == '/'):
+				result = a / b
+			elif(word == '*'):
+				result = a * b
+			elif(word == '^'):
+				result = a ** b
+			stack.append(result)
+
+	print(int(stack.pop()))
